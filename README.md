c43b79889facf189545fe6e11252d67b68b6ff6e
Kieran Browne <kierantbrowne@gmail.com>
2016-01-28 23:34:39 +1100

    initial

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Solar
+
+```scss
+@import "solar";
+$container: 800px;
+
+@include gen-grid($col:12,$gut:50);
+```
+
+```html
+<div class="grid-12-50">
+    <div class="span-4">Elit ad ipsum voluptate?</div>
+    <div class="span-4">Dolor odio maiores ad.</div>
+    <div class="span-4">Sit nobis blanditiis a!</div>
+    <div class="span-4">Ipsum adipisci voluptatum repellat.</div>
+    <div class="span-4">Adipisicing molestiae blanditiis reprehenderit?</div>
+    <div class="span-4">Sit aspernatur optio sit.</div>
+</div>
+```
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div class="grid-12-50">
+
+    <div class="span-6">Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!</div>
+
+    <div class="span-6">Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!</div>
+
+
+    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
+
+    <div class="span-7">
+      
+      <div>Sit itaque officia voluptatibus accusamus quas iure obcaecati est. Modi pariatur blanditiis.</div>
+      <div>Sit itaque officia voluptatibus accusamus quas iure obcaecati est. Modi pariatur blanditiis.</div>
+
+    </div>
+
+
+
+
+    <div class="span-2">Adipisicing facere recusandae voluptatibus</div>
+    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
+    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
+    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
+    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
+    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
+    <div class="span-9">Lorem nam et saepe ad ad! Quidem ex cupiditate quas voluptas reprehenderit. Dolorum dolorum impedit.</div>
+
+    <div class="span-12">Sit ea eos cumque voluptates ullam aperiam voluptate quisquam delectus repudiandae. Consequuntur eaque nesciunt voluptas!</div>
+
+  </div>
+
+
+  <?/*O('div.grid-12-50>div.span-6*6')*/?>
+
+</body>
+</html>
