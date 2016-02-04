8991cb3da8c10271bde7a2d594744205c6412eb7
Kieran Browne <kierantbrowne@gmail.com>
2016-02-05 00:15:58 +1100

    experimenting with base line grid, sizer, and baseline defaults mixin implemented. Tested against 5px grid; it seems to work

--- index.php
+++ index.php
@@ -4,59 +4,91 @@
   <meta charset="UTF-8">
   <title></title>
   <link rel="stylesheet" href="style.css">
+  <link href='https://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
+  <div class="grid-base">
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+    <div class="span-12"></div>
+  </div>
+
   <div class="grid guts">
 
-    <div class="span-6">Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!</div>
-
-    <div class="span-6">Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!</div>
-
-
-    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
-
-    <div class="span-9">
-      Sit itaque officia voluptatibus accusamus quas iure obcaecati est. Modi pariatur blanditiis.
-    </div>
-
-    <div class="span-12">
-      Sit itaque officia voluptatibus accusamus quas iure obcaecati est. Modi pariatur blanditiis.
-    </div>
-  </div>
-
-
-  <div class="grid-second">
-
-    <div class="span-6">Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!</div>
-
-    <div class="span-6">Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!</div>
-
-
-    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
-
-    <div class="span-7">
+    <div class="row">
+      <div class="span-3">
+        <h1>Amet explicabo.</h1>
+        <p>
+          Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!
+        </p>
+      </div>
       
-      <div>Sit itaque officia voluptatibus accusamus quas iure obcaecati est. Modi pariatur blanditiis.</div>
-      <div>Sit itaque officia voluptatibus accusamus quas iure obcaecati est. Modi pariatur blanditiis.</div>
-
+      <div class="span-3">
+        <h1>Lorem vero optio iste.</h1>
+        <p>
+          Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!
+        </p>
+      </div>
+      
+      <div class="span-6">
+        <h1>Sit obcaecati blanditiis.</h1>
+        <p>
+          Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!
+        </p>
+      </div>
     </div>
 
 
-
-
-    <div class="span-2">Adipisicing facere recusandae voluptatibus</div>
-    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
-    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
-    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
-    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
-    <div class="span-3">Adipisicing facere recusandae voluptatibus minus voluptate possimus. Officiis odio dignissimos atque tempore nemo debitis vel.</div>
-    <div class="span-9">Lorem nam et saepe ad ad! Quidem ex cupiditate quas voluptas reprehenderit. Dolorum dolorum impedit.</div>
-
-    <div class="span-12">Sit ea eos cumque voluptates ullam aperiam voluptate quisquam delectus repudiandae. Consequuntur eaque nesciunt voluptas!</div>
-
+    <div class="row">
+      <div class="span-3">
+        <h1>Elit cumque officia.</h1>
+        <p>
+          Amet totam explicabo tenetur iste aperiam assumenda similique est? Laborum soluta eligendi asperiores minima magni!
+        </p>
+      </div>
+    </div>
   </div>
-  <?/*O('div.grid-12-50>div.span-6*6')*/?>
+
+
+  <?/*O('div.grid>div.span-6*6')*/?>
 
 </body>
 </html>
